c70f0eb1770d3ca3dac235f3f7009192b0bc9e91
zefr0x <zer0-x.7ty50@aleeas.com>
2023-10-06 12:30:38 +0300

    Create gradle project

---
+++ app/src/main/java/carshow/App.java
@@ -0,0 +1,6 @@
+package carshow;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ app/src/test/java/carshow/AppTest.java
@@ -0,0 +1,7 @@
+package carshow;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+}

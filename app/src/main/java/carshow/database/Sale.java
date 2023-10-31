b3f881318d29a80e56933f95283c7aa7643b1046
zefr0x <zer0-x.7ty50@aleeas.com>
2023-10-31 03:21:24 +0300

    Add methods and properities to Sale class

--- app/src/main/java/carshow/database/Sale.java
+++ app/src/main/java/carshow/database/Sale.java
@@ -1,7 +1,16 @@
 package carshow.database;
 
+import java.util.Date;
+
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+
+enum PaymentMethod {
+    Cache,
+    Visa,
+    Mada,
+    Mastercard
+}
 
 @DatabaseTable(tableName = "sales")
 class Sale {
@@ -9,6 +18,9 @@ class Sale {
     public static final String COSTOMER_ID_FIELD = "costomer_id";
     public static final String SALES_MAN_ID_FIELD = "sales_man_id";
     public static final String PRODUCT_ID_FIELD = "product_id";
+    public static final String SALE_TIME_FIELD = "time";
+    public static final String TOTAL_BILL_FIELD = "total_bill";
+    public static final String PAYMENT_METHOD_FIELD = "payment_method";
 
     @DatabaseField(generatedId = true)
     private int id;
@@ -22,7 +34,14 @@ class Sale {
     @DatabaseField(columnName = PRODUCT_ID_FIELD, foreign = true)
     private ProductTypesWrapper product;
 
-    // TODO: Define sale parameters.
+    @DatabaseField(columnName = SALE_TIME_FIELD)
+    private long saleTime; // In Unix time stamp
+
+    @DatabaseField(columnName = TOTAL_BILL_FIELD)
+    private double totalBill;
+
+    @DatabaseField(columnName = PAYMENT_METHOD_FIELD)
+    private PaymentMethod paymentMethod;
 
     Sale() {
     }
@@ -37,5 +56,19 @@ class Sale {
         return this.id;
     }
 
-    // TODO: Implement sale methods.
+    public double getTotalBill() {
+        return this.totalBill;
+    }
+
+    public long getSaleTime() {
+        return this.saleTime;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return this.paymentMethod;
+    }
+
+    public String getSaleDateString() {
+        return new Date(this.saleTime).toString();
+    }
 }

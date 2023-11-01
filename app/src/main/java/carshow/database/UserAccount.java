69eddd02e544e9dd0e56550a961598e6cb42e16d
zefr0x <zer0-x.7ty50@aleeas.com>
2023-11-01 23:54:49 +0300

    Add poperities and methods for UserAccount classes

--- app/src/main/java/carshow/database/UserAccount.java
+++ app/src/main/java/carshow/database/UserAccount.java
@@ -9,6 +9,8 @@ class UserAccount {
     public static final String USER_NAME_FIELD = "user_name";
     public static final String PASSWORD_HASH_FIELD = "password_hash";
     public static final String PASSWORD_SALT_FIELD = "password_salt";
+    public static final String FIRST_NAME_FIELD = "first_name";
+    public static final String LAST_NAME_FIELD = "last_name";
 
     @DatabaseField(generatedId = true)
     private int id;
@@ -19,15 +21,23 @@ class UserAccount {
     @DatabaseField(columnName = PASSWORD_HASH_FIELD, canBeNull = false)
     private String password_hash;
 
+    @DatabaseField(columnName = FIRST_NAME_FIELD, canBeNull = false)
+    private String firstName;
+
+    @DatabaseField(columnName = LAST_NAME_FIELD, canBeNull = false)
+    private String lastName;
+
     @DatabaseField(columnName = PASSWORD_SALT_FIELD, canBeNull = false)
     private String password_salt;
 
-    UserAccount(String userName, String password) {
+    UserAccount(String userName, String password, String firstName, String lastName) {
         this.setUserName(userName);
         this.setPassword(password);
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
-    public void setUserName(String userName) {
+    private void setUserName(String userName) {
         // TODO: Apply restrictions on user names.
         this.userName = userName;
     }
@@ -46,6 +56,10 @@ class UserAccount {
         return this.userName;
     }
 
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
     public boolean validatePassword(String password) {
         return BCrypt.checkpw(password, this.password_hash);
     }
@@ -53,45 +67,140 @@ class UserAccount {
 
 @DatabaseTable(tableName = "admins")
 class AdminAccount extends UserAccount {
-    // TODO: Define admin specific parameters.
+    public static final String BRANCH_FIELD = "branch";
+    public static final String SALARY_FIELD = "salary";
+    public static final String OFFICE_NUMBER_FIELD = "office_number";
+
+    @DatabaseField(columnName = BRANCH_FIELD, canBeNull = false)
+    private String branch;
+
+    @DatabaseField(columnName = SALARY_FIELD, canBeNull = false)
+    private double salary;
+
+    @DatabaseField(columnName = OFFICE_NUMBER_FIELD, canBeNull = false)
+    private int officeNumber;
 
     AdminAccount() {
-        super("admin", "password");
+        this("admin", "password", "", "", "", 0.0, 0);
     }
 
-    AdminAccount(String userName, String password) {
-        super(userName, password);
+    AdminAccount(String userName, String password, String firstName, String lastName, String branch, double salary,
+            int officeNumber) {
+        super(userName, password, firstName, lastName);
+
+        setBranch(branch);
+        setSalary(salary);
+        setOfficeNumber(officeNumber);
+
     }
 
-    // TODO: Implement admin specific metionds.
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setOfficeNumber(int officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    public String getBranch() {
+        return this.branch;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public int getOfficeNumber() {
+        return this.officeNumber;
+    }
 }
 
 @DatabaseTable(tableName = "sales_men")
 class SalesManAccount extends UserAccount {
-    // TODO: Define sales man specific parameters.
+    public static final String BRANCH_FIELD = "branch";
+    public static final String SALARY_FIELD = "salary";
+    public static final String PRODUCT_TYPE_FIELD = "product_type";
+
+    @DatabaseField(columnName = BRANCH_FIELD, canBeNull = false)
+    private String branch;
+
+    @DatabaseField(columnName = SALARY_FIELD, canBeNull = false)
+    private double salary;
+
+    @DatabaseField(columnName = PRODUCT_TYPE_FIELD, canBeNull = false)
+    private ProductType productType;
 
     SalesManAccount() {
-        super("admin", "password");
+        this("admin", "password", "", "", "", 0.0, ProductType.Car);
     }
 
-    SalesManAccount(String userName, String password) {
-        super(userName, password);
+    SalesManAccount(String userName, String password, String firstName, String lastName, String branch, double salary,
+            ProductType productType) {
+        super(userName, password, firstName, lastName);
     }
 
-    // TODO: Implement sales man specific metionds.
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setProductType(ProductType productType) {
+        this.productType = productType;
+    }
+
+    public String getBranch() {
+        return this.branch;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public ProductType getProductType() {
+        return this.productType;
+    }
 }
 
 @DatabaseTable(tableName = "costomers")
 class CostomerAccount extends UserAccount {
-    // TODO: Define user specific parameters.
+    public static final String PHONE_NUMBER_FIELD = "phone";
+    public static final String EMAIL_ADDRESS_FIELD = "email";
+
+    @DatabaseField(columnName = PHONE_NUMBER_FIELD, canBeNull = false)
+    private String phoneNumber;
+
+    @DatabaseField(columnName = EMAIL_ADDRESS_FIELD, canBeNull = false)
+    private String emailAddress;
 
     CostomerAccount() {
-        super("costomer", "password");
+        this("costomer", "password", "", "", "", "");
     }
 
-    CostomerAccount(String userName, String password) {
-        super(userName, password);
+    CostomerAccount(String userName, String password, String firstName, String lastName, String phoneNumber,
+            String emailAddress) {
+        super(userName, password, firstName, lastName);
     }
 
-    // TODO: Implemente user specific metionds.
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return this.emailAddress;
+    }
 }
